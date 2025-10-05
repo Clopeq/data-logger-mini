405217ed7cfbdbb35c15315718bd97b3fafb9117
Clopeq <seba.krol07@gmail.com>
2025-10-05 17:29:29 +0200

    SPI testing

--- main.c
+++ main.c
@@ -58,11 +58,11 @@ int main(void)
         #define ChannelNumber 5
         UBYTE ChannelList[ChannelNumber] = {0, 1, 2, 3, 4};    // The channel must be less than 10
             
-        UDOUBLE Value[ChannelNumber] = {0};
+        //UDOUBLE Value[ChannelNumber] = {0};
         while(1) {
-            ADS1263_GetAll(ChannelList, Value, ChannelNumber);  // Get ADC1 value
+            //ADS1263_GetAll(ChannelList, Value, ChannelNumber);  // Get ADC1 value
             for(i=0; i<ChannelNumber; i++) {
-                printf("IN%d is %lf \n", ChannelList[i], ADC_read(ChannelList[i]));
+                printf("IN%d is %ld \n", ChannelList[i], ADC_read(ChannelList[i]));
                 // if((Value[i]>>31) == 1)
                 //     printf("IN%d is -%lf \r\n", ChannelList[i], REF*2 - Value[i]/2147483648.0 * REF);      //7fffffff + 1
                 // else
