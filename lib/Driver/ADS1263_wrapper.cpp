ceb5aefdeaef6a5f2df8a7ec33efa4988ecbe944
Clopeq <seba.krol07@gmail.com>
2025-10-12 18:37:28 +0200

    Add loadcell class

--- lib/Driver/ADS1263_wrapper.cpp
+++ lib/Driver/ADS1263_wrapper.cpp
@@ -11,11 +11,11 @@ UBYTE ADC2_init(ADS1263_ADC2_DRATE rate){
 void ADC_setmode(UBYTE Mode) {
     ADS1263_SetMode(Mode);
 }
-double ADC_read(short int Channel) {
+UDOUBLE ADC_read(short int Channel) {
     UDOUBLE value = ADS1263_GetChannalValue((UBYTE)Channel);
     if((value>>31) == 1)
          value = -1.0 * (REF*2.0 - value/2147483648.0 * REF);      //7fffffff + 1
     else
          value = value/2147483647.0 * REF;       //7fffffff
-    return (double)value;
+    return value;
 }
