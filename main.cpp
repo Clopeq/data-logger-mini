388cf42b6d4fdd7dca6f125a166649c72abd4a9c
Clopeq <seba.krol07@gmail.com>
2025-10-12 18:38:20 +0200

    Add loadcell class

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ int main(void)
     Loadcell LC1(ch);
 
     while(true) {
-        printf("%lf \n", ADC_read(0));
+        printf("%lf \n", (double)ADC_read(0));
         printf("\33[1A");
     }  
 
