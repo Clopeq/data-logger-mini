ffbd11fd7a4e5c71a8b4db98bdff4924c8cbc243
Clopeq <seba.krol07@gmail.com>
2025-10-22 19:36:38 +0200

    ADC refactoring

--- lib/ADC/include/ADC.h
+++ lib/ADC/include/ADC.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <cstdint>
-#include <cstddef>   // size_t is std::size_t
-
 class ADC {
 public:
-    ADC();                     
-    ~ADC();                    
+    ADC();
+    ~ADC();
 
-    uint8_t read_register(uint8_t reg_addr);
+    // Read a register from ADS1263
+    unsigned char read_register(unsigned char reg_addr);
 
 private:
-    int spi_fd;                
-    void spi_transfer(uint8_t* tx, uint8_t* rx, std::size_t len); // use std::size_t
+    int spi_fd;  // file descriptor for /dev/spidev0.0
+
+    // Perform SPI transfer (3 bytes)
+    void spi_transfer(unsigned char tx[3], unsigned char rx[3]);
 };
--- lib/ADC/src/ADC.cpp
+++ lib/ADC/src/ADC.cpp
@@ -1,25 +1,18 @@
 #include "ADC.h"
-#include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
-#include <cstring>
-#include <stdexcept>
-#include <cstddef>
 
 ADC::ADC() {
-    const char* device = "/dev/spidev0.0";
-    spi_fd = open(device, O_RDWR);
-    if (spi_fd < 0)
-        throw std::runtime_error("Failed to open SPI device");
+    spi_fd = open("/dev/spidev0.0", O_RDWR);
+    if (spi_fd < 0) return;  // fail silently (or you can print error)
 
-    uint8_t mode = SPI_MODE_1;
-    uint32_t speed = 1000000; // 1 MHz
-    if (ioctl(spi_fd, SPI_IOC_WR_MODE, &mode) < 0)
-        throw std::runtime_error("Can't set SPI mode");
-    if (ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0)
-        throw std::runtime_error("Can't set SPI speed");
+    unsigned char mode = SPI_MODE_1;
+    unsigned int speed = 1000000; // 1 MHz
+
+    ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
+    ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 }
 
 ADC::~ADC() {
@@ -27,23 +20,23 @@ ADC::~ADC() {
         close(spi_fd);
 }
 
-void ADC::spi_transfer(uint8_t* tx, uint8_t* rx, std::size_t len) {
-    struct spi_ioc_transfer tr{};
+void ADC::spi_transfer(unsigned char tx[3], unsigned char rx[3]) {
+    struct spi_ioc_transfer tr = {};
     tr.tx_buf = (unsigned long)tx;
     tr.rx_buf = (unsigned long)rx;
-    tr.len = len;
+    tr.len = 3;
     tr.speed_hz = 1000000;
     tr.bits_per_word = 8;
     tr.delay_usecs = 0;
 
-    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr) < 1)
-        throw std::runtime_error("SPI transfer failed");
+    ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr);
 }
 
-uint8_t ADC::read_register(uint8_t reg_addr) {
-    // Build command: RREG = 0x20 | (reg & 0x1F), read 1 register
-    uint8_t tx[] = { static_cast<uint8_t>(0x20 | (reg_addr & 0x1F)), 0x00, 0x00 };
-    uint8_t rx[sizeof(tx)] = {0};
-    spi_transfer(tx, rx, sizeof(tx));
-    return rx[2]; // Third byte contains the register value
+unsigned char ADC::read_register(unsigned char reg_addr) {
+    unsigned char tx[3] = { static_cast<unsigned char>(0x20 | (reg_addr & 0x1F)), 0x00, 0x00 };
+    unsigned char rx[3] = {0};
+
+    spi_transfer(tx, rx);
+
+    return rx[2];  // third byte contains the register value
 }
