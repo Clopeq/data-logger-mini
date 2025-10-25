5c7626e6e6e6d1d7ceaabc40dec6a6274de9d63c
Clopeq <seba.krol07@gmail.com>
2025-10-25 16:18:21 +0200

    wiring Pi implementation

--- lib/ADC/src/ADC.cpp
+++ lib/ADC/src/ADC.cpp
@@ -7,6 +7,9 @@
 
 static int spi_fd = -1; // internal global
 
+const float VREF = 5.08;    // ADS1263 reference voltage
+const int GAIN = 1;         // PGA gain
+
 void ADC_init() {
     spi_fd = open("/dev/spidev0.0", O_RDWR);
     unsigned char mode = SPI_MODE_1;
@@ -19,7 +22,7 @@ void ADC_close() {
     if (spi_fd >= 0) close(spi_fd);
 }
 
-unsigned char ADC_read(unsigned char reg_addr) {
+unsigned char ADC_read_register(unsigned char reg_addr) {
     unsigned char tx[3] = { static_cast<unsigned char>(0x20 | (reg_addr & 0x1F)), 0x00, 0x00 };
     unsigned char rx[3] = {0};
     struct spi_ioc_transfer tr{};
