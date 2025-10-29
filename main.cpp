78e2d5a41a398837fada3ee36f09bcca1d731d98
Clopeq <seba.krol07@gmail.com>
2025-10-29 22:52:31 +0100

    showcase

--- main.cpp
+++ main.cpp
@@ -15,65 +15,65 @@ void clearConsole() {
     cout << "\033[2J\033[1;1H"; // ANSI escape codes
 }
 
-// void ADC_test() {
+void ADC_test() {
     
-//     ADS1263 ADC(500*1000);
+    ADS1263 ADC(500*1000);
 
-//     ADC.set_gain(GAIN_1);
-//     ADC.set_data_rate(SPS_400);
+    ADC.set_gain(GAIN_1);
+    ADC.set_data_rate(SPS_400);
 
-//     PortManager CH0(A0, COMM, 0, 1);
+    PortManager CH0(A0, COMM, 0, 1);
     
-//     string command;
+    string command;
 
-//     double new_measurement;
+    double new_measurement;
 
-//     clearConsole();
-//     while(true) {
-//         cout << "CMD: ";
-//         cin >> command;
-//         clearConsole();
+    clearConsole();
+    while(true) {
+        cout << "CMD: ";
+        cin >> command;
+        clearConsole();
 
-//         if(command == "EXIT") {
-//             cout << "EXIT!" << endl;
-//             return;
-//         } else if(command == "M") {
-//             for(int i=0; i<=10; i++) {
-//                 cout << i << ": " << ADC.read(CH0) << endl;
-//                 sleep(10);
-//             }
-//         } else if(command == "CAL") {
-//             cout << "Current measurement: " << ADC.read(CH0) << endl;
-//             cout << "Current cal_factor: " << CH0.get_cal_factor() << endl;
-//             cout << "new measurement: ";
-//             cin >> new_measurement;
-//             CH0.calibrate(ADC.read(CH0), new_measurement);
-//             cout << "new measurement: " << ADC.read(CH0) << endl;
-//             cout << "new cal_factor: " << CH0.get_cal_factor() << endl;
+        if(command == "EXIT") {
+            cout << "EXIT!" << endl;
+            return;
+        } else if(command == "M") {
+            for(int i=0; i<=10; i++) {
+                cout << i << ": " << ADC.read(CH0) << endl;
+                sleep(10);
+            }
+        } else if(command == "CAL") {
+            cout << "Current measurement: " << ADC.read(CH0) << endl;
+            cout << "Current cal_factor: " << CH0.get_cal_factor() << endl;
+            cout << "new measurement: ";
+            cin >> new_measurement;
+            CH0.calibrate(ADC.read(CH0), new_measurement);
+            cout << "new measurement: " << ADC.read(CH0) << endl;
+            cout << "new cal_factor: " << CH0.get_cal_factor() << endl;
 
 
-//         } else if(command == "TARE") {
-//             cout << "Current measurement: " << ADC.read(CH0) << endl;
-//             cout << "Current tare_factor: " << CH0.get_tare_factor() << endl;
-//             cout << "new measurement: ";
-//             cin >> new_measurement;
-//             CH0.tare(ADC.read(CH0), new_measurement);
-//             cout << "new measurement: " << ADC.read(CH0) << endl;
-//             cout << "new tare_factor: " << CH0.get_tare_factor() << endl;
+        } else if(command == "TARE") {
+            cout << "Current measurement: " << ADC.read(CH0) << endl;
+            cout << "Current tare_factor: " << CH0.get_tare_factor() << endl;
+            cout << "new measurement: ";
+            cin >> new_measurement;
+            CH0.tare(ADC.read(CH0), new_measurement);
+            cout << "new measurement: " << ADC.read(CH0) << endl;
+            cout << "new tare_factor: " << CH0.get_tare_factor() << endl;
 
-//         } else {
-//             cout << "Unknown command" << endl;
-//         }
+        } else {
+            cout << "Unknown command" << endl;
+        }
 
-//     }
-// }
+    }
+}
 
 int main() {
 
 
-    cout << get_config_dir() << endl;
+    //cout << get_config_dir() << endl;
 
-
+    ADC_test();
 
     return 0;
 }
